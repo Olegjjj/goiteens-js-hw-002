1aab39bc5772253aa3ff619375aef62ad15a2ffc
Oleg Plaksin <plaksinoleg53@gmail.com>
2023-06-11 18:46:43 +0300

    git

---
+++ index.js
@@ -0,0 +1,35 @@
+const celsius = 25;
+const fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit)
+
+const month = 30;
+const hours = month * 24;
+let minutes = 24 * 60;
+console.log(month)
+console.log(hours)
+console.log(minutes * month)
+
+const health = 100;
+const energy = 100;
+console.log(health - 20);
+console.log(energy - 20);
+
+const isCost = 1200;
+console.log(isCost / 100 * 10);
+
+const myNumber = 1.7;
+console.log(Math.floor(myNumber)); 
+
+const myString = "12.46";
+console.log(Number.parseFloat(myString));
+
+const isString = "12";
+console.log(Number.parseInt(isString)); 
+
+const isNumber = 9;
+console.log(Math.sqrt(isNumber))
+
+const thisNumber = 1;
+const thisString = "1";
+console.log(Number.parseInt(thisString)); 
+console.log(thisNumber.toString());
